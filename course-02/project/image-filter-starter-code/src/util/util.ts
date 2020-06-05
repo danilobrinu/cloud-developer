94db5ccbcc3f31e996d7fad6ff4a463c379b614e
danilobrinu <ddbn.c2@gmail.com>
2020-06-04 19:16:01 -0500

    Add validateURL method and refactor filterImageFromURL

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import Jimp = require('jimp');
+import fs from "fs";
+import Jimp from "jimp";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -8,27 +8,50 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
+export const filterImageFromURL = async (inputURL: string): Promise<string> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const uid = Math.floor(Math.random() * new Date().getTime())
+        .toString(16)
+        .replace(".", "");
+      const outpath = `/tmp/filtered.${uid}.jpg`;
+      const targetpath = __dirname + outpath;
+      await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
-}
+        .writeAsync(targetpath);
+
+      resolve(targetpath);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 // deleteLocalFiles
 // helper function to delete files on the local disk
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
+export const deleteLocalFiles = async (files: Array<string>) => {
+  for (let file of files) {
+    try {
+      fs.unlinkSync(file);
+    } catch (e) {
+      console.error(e);
     }
-}
+  }
+};
+
+export const validateURL = (url: string): Boolean => {
+  if (
+    !/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+      url
+    )
+  ) {
+    return false;
+  }
+  return true;
+};
